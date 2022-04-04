e7dd82153ff6ce27fd43e5098aa91b153934f204
ynportfolios <y.n.portfolios@gmail.com>
2022-04-04 11:31:15 +0900

    Implement crud todo

---
+++ app/controllers/v1/todos_controller.rb
@@ -0,0 +1,28 @@
+class V1::TodosController < ApplicationController
+  def index
+    todo = Todo.all
+    render json: todo
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo
+    else
+      render json: todo.errors
+    end
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    if todo.destroy
+      render json: todo
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title)
+  end
+end
